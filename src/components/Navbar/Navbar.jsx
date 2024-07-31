1f5de8ff09ade276a1fd455696a99a79d6829f51
Michael voevudskiy <michael.voevudskiy@gmail.com>
2024-07-31 10:53:46 +0300

    deploy_2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   const st = navData => navData.isActive ? 'active' : 'menu'
   return <div className='Navbar'>
     <NavLink className='menu' to='/'>Main Page</NavLink>
-    <NavLink to='/Eq' className={st}>Калькулятор еквідистанти</NavLink>
-    <NavLink to='/Сhamfers' className={st}>Калькулятор Фасок</NavLink>
+    <NavLink to='/Eq' className={st}>Еквідистанта</NavLink>
+    <NavLink to='/Сhamfers' className={st}>Фаски</NavLink>
     <NavLink to='/Weight' className={st}>Вага</NavLink>
   </div>
 }
